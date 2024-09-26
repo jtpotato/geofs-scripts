a44b7f6fef22b6185e15bef76f92824c8c0eca0a
Joel <58995538+jtpotato@users.noreply.github.com>
2024-09-27 08:22:26 +1000

    add tiller fix

---
+++ GeoFS Tiller Fix.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         GeoFS Tiller Fix
+// @namespace    GeoFS
+// @version      1.0
+// @description  Dampens the tiller at high speeds to avoid being out-of control on the runway.
+// @author       jtpotato
+// @match        https://*.geo-fs.com/geofs.php*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  setInterval(function () {
+    if (
+      geofs.aircraft.instance.groundContact &&
+      geofs.aircraft.instance.groundSpeed > 10
+    ) {
+      // console.log(controls.yaw);
+      if (Math.abs(controls.yaw) > 2 / geofs.aircraft.instance.groundSpeed) {
+        controls.yaw =
+          (2 / geofs.aircraft.instance.groundSpeed) * Math.sign(controls.yaw);
+      }
+    }
+  }, 6); // somewhat faster than framerate will be ok
+})();
